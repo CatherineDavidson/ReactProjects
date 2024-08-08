88fecc1d53ffed1e381ac9649fab2936ca2bd7f0
CatherineDavidson <144999318+CatherineDavidson@users.noreply.github.com>
2024-08-08 15:55:36 +0530

    Add files via upload

---
+++ DAY2/PAH3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Smile()
+{
+    return(
+        <div style={{textAlign: 'center'}}>
+            <h1>Smile Component</h1>
+            <p>It is a functional coponent</p>
+            <div >
+                <img src="https://static.vecteezy.com/system/resources/previews/003/660/834/original/happy-smile-emoticon-expression-free-vector.jpg" alt="" height={'300px'} width={'300px'}/>
+            </div>
+        </div>
+    );
+};
+export default Smile;
